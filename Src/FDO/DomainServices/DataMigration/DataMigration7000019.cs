sillsdev/FieldWorks

Make FDO dependencies UI independent
This includes SilUtils, BasicUtils, and COMInterfaces. COMInterfaces
still depends on System.Drawing, but not on System.Windows.Forms.

Most files got moved around, but some classes got replaced with
corresponding classes from SIL.Core (e.g. Logger; StringUtils [which
is named StringHelper in libpalaso]).

Change-Id: I6476f44b47cc2ec3e3f37e696fe5030c3a0f58e8

Src/FDO/DomainServices/DataMigration/DataMigration7000019.cs
@@ -14,6 +14,7 @@ using System.Xml;
 using System.Xml.Linq;
 using SIL.CoreImpl;
 using SIL.FieldWorks.Common.COMInterfaces;
+using SIL.Reporting;
 using SIL.WritingSystems;
 using SIL.WritingSystems.Migration;
 using SIL.Xml;
@@ -235,7 +236,7 @@ namespace SIL.FieldWorks.FDO.DomainServices.DataMigration
 				if (String.IsNullOrEmpty(wsVal))
 				{
 					wsVal = "en";
-					SIL.Utils.Logger.WriteEvent(String.Format(
+					Logger.WriteEvent(String.Format(
 						"Converting empty ws attribute to ws=\"en\" in DataMigration7000019: ParentNode={1}{0}",
 						elem.Parent.ToString(), Environment.NewLine));
 				}
@@ -252,7 +253,7 @@ namespace SIL.FieldWorks.FDO.DomainServices.DataMigration
 				if (String.IsNullOrEmpty(wsVal))
 				{
 					wsVal = "en";
-					SIL.Utils.Logger.WriteEvent(String.Format(
+					Logger.WriteEvent(String.Format(
 						"Converting empty wsBase attribute to ws=\"en\" in DataMigration7000019: ParentNode={1}{0}.",
 						elem.Parent.ToString(), Environment.NewLine));
 				}

Src/FDO/DomainServices/DomainObjectServices.cs
@@ -10,14 +10,15 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Xml;
+using SIL.CoreImpl;
 using SIL.FieldWorks.Common.COMInterfaces;
 using SIL.FieldWorks.Common.ScriptureUtils;
-using SIL.Utils;
 using SIL.FieldWorks.FDO.Application;
-using SIL.FieldWorks.FDO.Infrastructure;
-using SILUBS.SharedScrUtils;
-using SIL.CoreImpl;
 using SIL.FieldWorks.FDO.DomainImpl;
+using SIL.FieldWorks.FDO.Infrastructure;
+using SIL.Reporting;
+using SIL.Utils;
+using SILUBS.SharedScrUtils;
 
 namespace SIL.FieldWorks.FDO.DomainServices
 {

Src/FDO/Infrastructure/Impl/FdoChangesUnitOfWork.cs
@@ -10,7 +10,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using SIL.FieldWorks.Common.COMInterfaces;
-using SIL.Utils;
+using SIL.Reporting;
 
 namespace SIL.FieldWorks.FDO.Infrastructure.Impl
 {

Src/FDO/Infrastructure/Impl/XMLBackendProvider.cs
@@ -13,10 +13,11 @@ using System.Linq;
 using System.Text;
 using System.Xml;
 using System.Xml.Linq;
-using SIL.IO.FileLock;
 using SIL.CoreImpl;
 using SIL.FieldWorks.FDO.DomainServices.DataMigration;
+using SIL.IO.FileLock;
 using SIL.Lexicon;
+using SIL.Reporting;
 using SIL.Utils;
 
 namespace SIL.FieldWorks.FDO.Infrastructure.Impl

sillsdev/liblcm

(cherry picked from commit 5a77a746cd3dfc8e4591dfebbbe0d0a1ce9f931d)
Conflicts:
	Src/Common/COMInterfaces/COMInterfacesTests/IcuWrappersTests.cs
	Src/Common/Controls/DetailControls/MorphTypeAtomicLauncher.cs
	Src/Common/Controls/DetailControls/PictureSlice.cs
	Src/Common/Controls/DetailControls/SummarySlice.cs
	Src/Common/Controls/Widgets/FontHeightAdjuster.cs
	Src/Common/Controls/Widgets/FwComboBox.cs
	Src/Common/Controls/Widgets/FwTextBox.cs
	Src/Common/Controls/XMLViews/BulkEditBar.cs
	Src/Common/Controls/XMLViews/FilterBar.cs
	Src/Common/Controls/XMLViews/XmlBrowseView.cs
	Src/Common/CoreImpl/CoreImplTests/WritingSystemManagerTests.cs
	Src/Common/CoreImpl/KeyboardHelper.cs
	Src/Common/FieldWorks/FieldWorks.cs
	Src/Common/FieldWorks/LexicalProvider/LexicalProviderImpl.cs
	Src/Common/FieldWorks/WelcomeToFieldWorksDlg.Designer.cs
	Src/Common/FieldWorks/WelcomeToFieldWorksDlg.cs
	Src/Common/Framework/FwApp.cs
	Src/Common/FwUtils/FwUtils.cs
	Src/Common/FwUtils/FwUtilsTests/DummyKeyboardAdaptor.cs
	Src/Common/SimpleRootSite/EditingHelper.cs
	Src/Common/SimpleRootSite/SimpleRootSite.cs
	Src/Common/SimpleRootSite/SimpleRootSiteTests/IbusRootSite-
            EventHandlerTests.cs
            EventHandlerTests_Simple.cs
	Src/Common/SimpleRootSite/SimpleRootSiteTests/SimpleRootSite-
            Tests_IsSelectionVisibleTests.cs
	Src/FDO/DomainServices/BackupRestore/ProjectRestoreService.cs
	Src/FDO/DomainServices/DataMigration/DataMigration7000019.cs
	Src/FDO/Infrastructure/Impl/XMLBackendProvider.cs
	Src/FdoUi/Dialogs/MergeObjectDlg.cs
	Src/FdoUi/gendarme-FdoUi.ignore
	Src/FwCoreDlgs/ArchiveWithRamp.cs
	Src/FwCoreDlgs/ChooseLangProjectDialog.cs
	Src/FwCoreDlgs/CnvtrPropertiesCtrl.cs
	Src/FwCoreDlgs/DuplicateProjectFoundDlg.cs
	Src/FwCoreDlgs/FwCoreDlgsTests/WritingSystemPropertiesDialogTests.cs
	Src/FwCoreDlgs/FwFindReplaceDlg.cs
	Src/FwCoreDlgs/FwHelpAbout.cs
	Src/FwCoreDlgs/FwNewLangProject.cs
	Src/FwCoreDlgs/FwProjPropertiesDlg.cs
	Src/FwCoreDlgs/RealSplashScreen.cs
	Sr

Src/FDO/DomainServices/DataMigration/DataMigration7000019.cs
@@ -11,6 +11,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Xml;
 using System.Xml.Linq;
+using Palaso.Reporting;
 using Palaso.WritingSystems;
 using Palaso.WritingSystems.Collation;
 using Palaso.Xml;
@@ -234,7 +235,7 @@ namespace SIL.FieldWorks.FDO.DomainServices.DataMigration
 				if (String.IsNullOrEmpty(wsVal))
 				{
 					wsVal = "en";
-					SIL.Utils.Logger.WriteEvent(String.Format(
+					Logger.WriteEvent(String.Format(
 						"Converting empty ws attribute to ws=\"en\" in DataMigration7000019: ParentNode={1}{0}",
 						elem.Parent.ToString(), Environment.NewLine));
 				}
@@ -251,7 +252,7 @@ namespace SIL.FieldWorks.FDO.DomainServices.DataMigration
 				if (String.IsNullOrEmpty(wsVal))
 				{
 					wsVal = "en";
-					SIL.Utils.Logger.WriteEvent(String.Format(
+					Logger.WriteEvent(String.Format(
 						"Converting empty wsBase attribute to ws=\"en\" in DataMigration7000019: ParentNode={1}{0}.",
 						elem.Parent.ToString(), Environment.NewLine));
 				}

Src/FDO/DomainServices/DomainObjectServices.cs
@@ -10,14 +10,15 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Xml;
+using SIL.CoreImpl;
 using SIL.FieldWorks.Common.COMInterfaces;
 using SIL.FieldWorks.Common.ScriptureUtils;
-using SIL.Utils;
 using SIL.FieldWorks.FDO.Application;
-using SIL.FieldWorks.FDO.Infrastructure;
-using SILUBS.SharedScrUtils;
-using SIL.CoreImpl;
 using SIL.FieldWorks.FDO.DomainImpl;
+using SIL.FieldWorks.FDO.Infrastructure;
+using Palaso.Reporting;
+using SIL.Utils;
+using SILUBS.SharedScrUtils;
 
 namespace SIL.FieldWorks.FDO.DomainServices
 {

Src/FDO/Infrastructure/Impl/FdoChangesUnitOfWork.cs
@@ -10,7 +10,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using SIL.FieldWorks.Common.COMInterfaces;
-using SIL.Utils;
+using Palaso.Reporting;
 
 namespace SIL.FieldWorks.FDO.Infrastructure.Impl
 {

Src/FDO/Infrastructure/Impl/XMLBackendProvider.cs
@@ -13,9 +13,10 @@ using System.Linq;
 using System.Text;
 using System.Xml;
 using System.Xml.Linq;
-using Palaso.IO.FileLock;
 using SIL.CoreImpl;
 using SIL.FieldWorks.FDO.DomainServices.DataMigration;
+using Palaso.IO.FileLock;
+using Palaso.Reporting;
 using SIL.Utils;
 
 namespace SIL.FieldWorks.FDO.Infrastructure.Impl