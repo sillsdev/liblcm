4d1ac4c84289c5637fa8997810e5a4dfc3b3b672
Manjula <manjula@ecgroup-intl.com>
2018-02-14 12:32:56 +0530

    Fix: LT-18917 Add Data Migration to DM70000072 in LCM
    * Created the Migration Class DataMigration7000072.cs
    * Created Test Conditions in DataMigration7000072Tests.cs

---
+++ tests/SIL.LCModel.Tests/DomainServices/DataMigration/DataMigration7000072Tests.cs
@@ -0,0 +1,75 @@
+﻿// Copyright (c) 2016-2017 SIL International
+// This software is licensed under the LGPL, version 2.1 or later
+// (http://www.gnu.org/licenses/lgpl-2.1.html)
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using NUnit.Framework;
+
+namespace SIL.LCModel.DomainServices.DataMigration
+{
+	/// <summary>
+	/// Unit tests for DataMigration7000072
+	/// </summary>
+	[TestFixture]
+	public class DataMigration7000072Tests : DataMigrationTestsBase
+	{
+		/// <summary>
+		/// Test the migration from version 7000071 to 7000072.
+		/// </summary>
+		[Test]
+		public void DataMigration7000072Test()
+		{
+			var dtos = DataMigrationTestServices.ParseProjectFile("DataMigration7000072.xml");
+			var mockMdc = new MockMDCForDataMigration();
+			IDomainObjectDTORepository dtoRepos = new DomainObjectDtoRepository(7000071, dtos, mockMdc, null,
+				TestDirectoryFinder.LcmDirectories);
+			m_dataMigrationManager.PerformMigration(dtoRepos, 7000072, new DummyProgressDlg());
+
+			// Test to Check whether the given Reversal Index Entry has the added senses
+			// Get the Reversal Index Entry with the specified guid
+			var aReversalIndexEntry = dtoRepos.GetDTO("2d1fb89c-5671-43de-8cdd-4624f4648ef4");
+			var reversalIndexElement = XElement.Parse(aReversalIndexEntry.Xml);
+
+			// Check for the Senses Tag
+			var sensesElement = reversalIndexElement.Element("Senses");
+			if (sensesElement != null)
+			{
+				var objsurElementsList = sensesElement.Elements("objsur");
+				var guidList = new ArrayList();
+				foreach (var objsurElement in objsurElementsList)
+				{
+					guidList.Add(objsurElement.Attribute("guid")?.Value);
+				}
+
+				Assert.AreEqual(guidList[0], "c836e945-92d3-4560-9622-bfd9656551c8");
+				Assert.AreEqual(guidList[1], "d3d19eae-d840-484e-8de2-0100336808ed");
+			}
+
+			// Test to check whether Reversal Entries collection has been removed from LexSense
+			var allLexSenses = dtoRepos.AllInstancesWithSubclasses("LexSense");
+			foreach (var aLexSense in allLexSenses)
+			{
+				var lexSenseElement = XElement.Parse(aLexSense.Xml);
+				var reveralEntriesElement = lexSenseElement.Element("ReversalEntries");
+				Assert.AreEqual(reveralEntriesElement,null);
+			}
+
+			// Test to check whether the Referring Senses value has been changed to Senses under VirtualOrdering
+			var allOrderings = dtoRepos.AllInstancesWithSubclasses("VirtualOrdering");
+			foreach (var anOrdering in allOrderings)
+			{
+				var orderingElement = XElement.Parse(anOrdering.Xml);
+				var field = orderingElement.Element("Field");
+				var uniValue = field.Element("Uni");
+
+				Assert.AreEqual("Senses", uniValue.Value);
+			}
+		}
+	}
+}
