b39da0cf04e2ddf37f6e28cd9737168034a79627
Eberhard Beilharz <eb1@sil.org>
2018-11-12 16:20:32 +0100

    Update version number

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -15,5 +15,5 @@ using System.Reflection;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyFileVersion("9.0.0.0")]
-[assembly: AssemblyVersion("9.0.0.0")]
+[assembly: AssemblyFileVersion("10.0.0.0")]
+[assembly: AssemblyVersion("10.0.0.0")]
