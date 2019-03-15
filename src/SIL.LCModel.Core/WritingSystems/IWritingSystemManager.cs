131a92ba519862cbad8dc95d8f141536a1cdadf8
Jason Naylor <jason_naylor@sil.org>
2019-03-15 15:33:04 -0500

    Add IWritingSystemManager interface
    * Will help with unit testing at the very least

---
+++ src/SIL.LCModel.Core/WritingSystems/IWritingSystemManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using SIL.WritingSystems;
+
+namespace SIL.LCModel.Core.WritingSystems
+{
+	public interface IWritingSystemManager
+	{
+		bool CanSave(CoreWritingSystemDefinition ws);
+		CoreWritingSystemDefinition Create(LanguageSubtag languageSubtag, ScriptSubtag scriptSubtag, RegionSubtag regionSubtag, IEnumerable<VariantSubtag> variantSubtags);
+		CoreWritingSystemDefinition Create(string ietfLanguageTag);
+		CoreWritingSystemDefinition CreateFrom(CoreWritingSystemDefinition ws);
+		bool Exists(int handle);
+		bool Exists(string identifier);
+		CoreWritingSystemDefinition Get(int handle);
+		CoreWritingSystemDefinition Get(string identifier);
+		void Replace(CoreWritingSystemDefinition ws);
+		void Save();
+		void Set(CoreWritingSystemDefinition ws);
+		CoreWritingSystemDefinition Set(string identifier);
+		bool TryGet(string identifier, out CoreWritingSystemDefinition ws);
+	}
+}
