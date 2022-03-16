b6bf43b489dfef2009d575f0acf65f1a92841b81
Joseph Myers <wjosephmyers@gmail.com>
2022-03-16 09:24:11 +0700

    Using Color per PR comment.

--- src/SIL.LCModel/DomainServices/WritingSystemServices.cs
+++ src/SIL.LCModel/DomainServices/WritingSystemServices.cs
@@ -94,9 +94,9 @@ namespace SIL.LCModel.DomainServices
 			get
 			{
 				var tpb = TsStringUtils.MakePropsBldr();
-				const int darkColor = unchecked((int) 0xFF716F64);
+				var darkColor = Color.FromArgb(64,64,64);
 				tpb.SetIntPropValues((int)FwTextPropType.ktptForeColor, (int)FwTextPropVar.ktpvDefault,
-					(int)ColorUtil.ConvertColorToBGR(Color.FromArgb(darkColor)));
+					(int)ColorUtil.ConvertColorToBGR(darkColor));
 				//				// This is the formula (red + (blue * 256 + green) * 256) for a FW RGB color,
 				//				// applied to the standard FW color "light blue". This is the default defn of the
 				//				// "Language Code" character style in DN. We could just use this style, except
