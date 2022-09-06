0b33700cc1f34c657aae4a767229ac7c8d20b496
Mark Kidder <mark_kidder@sil.org>
2022-09-06 11:50:57 -0400

    LT-21078: Expose the string "<Not Sure>" externally.
    https://jira.sil.org/browse/LT-21078

---
+++ src/SIL.LCModel/StringsUtil.cs
@@ -0,0 +1,18 @@
+// Copyright (c) 2022 SIL International
+// This software is licensed under the LGPL, version 2.1 or later
+// (http://www.gnu.org/licenses/lgpl-2.1.html)
+
+using System;
+
+namespace SIL.LCModel
+{
+	public class StringsUtil
+	{
+		/// ------------------------------------------------------------------------------------
+		/// <summary>
+		/// Expose the "<Not Sure>" string to external modules.
+		/// </summary>
+		/// ------------------------------------------------------------------------------------
+		public static string NotSure => Strings.ksNotSure;
+	}
+}
