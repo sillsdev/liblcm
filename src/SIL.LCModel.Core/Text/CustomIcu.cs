c6df40e5e2c1f54512152b53f5509a36882134a6
Hasso <hasso_pape@sil.org>
2022-12-13 10:31:09 -0600

    Fix Linux Tests by copying icu.net.dll.config
    per a comment in https://github.com/sillsdev/icu-dotnet/issues/181
    https://github.com/sillsdev/TheCombine/issues/1768
    
    Change-Id: Ia1b09ac7bc0b73670c2a6e10924ae4f94e3e9adf
    #issuecomment-1344645680

--- src/SIL.LCModel.Core/Text/CustomIcu.cs
+++ src/SIL.LCModel.Core/Text/CustomIcu.cs
@@ -230,12 +230,6 @@ namespace SIL.LCModel.Core.Text
 				HaveCustomIcuLibrary = false;
 				Debug.WriteLine("Can't find SilIcuInit() - no custom ICU installed?");
 			}
-			catch (EntryPointNotFoundException)
-			{
-				// this started happening on Linux in 2022.12; not sure why
-				HaveCustomIcuLibrary = false;
-				Debug.WriteLine("Can't enter SilIcuInit() - entry point not found!");
-			}
 			catch (BadImageFormatException)
 			{
 				// we found a custom ICU but with an incorrect format (e.g. x64 instead of x86)
