6a6f6b9d1e0c129f67841cc887f973926eea47b1
Jason Naylor <jason_naylor@sil.org>
2023-01-25 09:16:45 -0800

    Fix AbandonedMutex issue by using GlobalMutex API properly

--- tests/SIL.LCModel.Core.Tests/Scripture/BCVRefTests.cs
+++ tests/SIL.LCModel.Core.Tests/Scripture/BCVRefTests.cs
@@ -1,11 +1,10 @@
-// Copyright (c) 2011-2015 SIL International
+// Copyright (c) 2011-2023 SIL International
 // This software is licensed under the LGPL, version 2.1 or later
 // (http://www.gnu.org/licenses/lgpl-2.1.html)
 
 using System;
 using System.IO;
 using System.Reflection;
-using System.Threading;
 using NUnit.Framework;
 using SIL.Threading;
 
@@ -36,8 +35,8 @@ namespace SIL.LCModel.Core.Scripture
 			var originalFile = Path.Combine(vrsPath, VersificationTable.GetFileNameForVersification(ScrVers.Original));
 			// Avoid race conditions between parallel tests which need the versification initialized
 			using (var mutex = new GlobalMutex("VersificationTableSetupMutex"))
+			using (mutex.InitializeAndLock())
 			{
-				mutex.InitializeAndLock();
 				if (!File.Exists(englishFile)) // check only the English file existence for simplicity
 				{
 					File.WriteAllBytes(englishFile, Properties.TestResources.eng);
@@ -47,14 +46,16 @@ namespace SIL.LCModel.Core.Scripture
 				else
 				{
 					// This setup code assumes that we aren't modifying the files on disk after initialization
-					Assert.That(File.ReadAllBytes(englishFile), Is.EqualTo(Properties.TestResources.eng),
+					Assert.That(File.ReadAllBytes(englishFile),
+						Is.EqualTo(Properties.TestResources.eng),
 						"Error setting up VersificationTable data for Scripture Reference tests.");
-					Assert.That(File.ReadAllBytes(septuagintFile), Is.EqualTo(Properties.TestResources.lxx),
+					Assert.That(File.ReadAllBytes(septuagintFile),
+						Is.EqualTo(Properties.TestResources.lxx),
 						"Error setting up VersificationTable data for Scripture Reference tests.");
-					Assert.That(File.ReadAllBytes(originalFile), Is.EqualTo(Properties.TestResources.org),
+					Assert.That(File.ReadAllBytes(originalFile),
+						Is.EqualTo(Properties.TestResources.org),
 						"Error setting up VersificationTable data for Scripture Reference tests.");
 				}
-				mutex.Unlink();
 			}
 			VersificationTable.Initialize(vrsPath);
 		}
