e68ba4165f7e9c21554c24500cd5e0cc06a06901
Kevin Hahn <kevin_hahn@sil.org>
2024-07-26 10:29:09 +0700

    update CommonServiceLocator to remove Microsoft.NETCore.App dependancy (#302)
    * upgrade common service locator from 1.4.0 to 2.0.7. Replace use of IServiceLocator with IServiceProvider as this is now in the BCL. Write helper class to provide source compatability with IServiceLocator.
    
    * add helper method to get all instances of a service.

--- src/SIL.LCModel/DomainServices/M3ModelExportServices.cs
+++ src/SIL.LCModel/DomainServices/M3ModelExportServices.cs
@@ -7,7 +7,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Xml.Linq;
 using Icu;
-using Microsoft.Practices.ServiceLocation;
 using SIL.LCModel.Core.KernelInterfaces;
 using SIL.LCModel.Core.WritingSystems;
 
@@ -315,7 +314,7 @@ namespace SIL.LCModel.DomainServices
 		/// <summary>
 		/// Export the full lexicon when exporting both grammar and lexicon.
 		/// </summary>
-		private static XElement ExportLexiconFull(IServiceLocator servLoc, Normalizer.UNormalizationMode mode)
+		private static XElement ExportLexiconFull(IServiceProvider servLoc, Normalizer.UNormalizationMode mode)
 		{
 			return new XElement("Lexicon",
 					ExportEntries(servLoc.GetInstance<ILexEntryRepository>()),
@@ -349,7 +348,7 @@ namespace SIL.LCModel.DomainServices
 							select ExportItemAsReference(lexEntryType, "LexEntryType"))));
 		}
 
-		private static XElement ExportMsas(IServiceLocator servLoc)
+		private static XElement ExportMsas(IServiceProvider servLoc)
 		{
 			return new XElement("MorphoSyntaxAnalyses",
 				from stemMsa in servLoc.GetInstance<IMoStemMsaRepository>().AllInstances()
@@ -407,7 +406,7 @@ namespace SIL.LCModel.DomainServices
 					ExportBestAnalysis(sense.Definition, "Definition", mode)));
 		}
 
-		private static XElement ExportAllomorphs(IServiceLocator servLoc, Normalizer.UNormalizationMode mode)
+		private static XElement ExportAllomorphs(IServiceProvider servLoc, Normalizer.UNormalizationMode mode)
 		{
 			return new XElement("Allomorphs",
 				from stemAllo in servLoc.GetInstance<IMoStemAllomorphRepository>().AllInstances()
