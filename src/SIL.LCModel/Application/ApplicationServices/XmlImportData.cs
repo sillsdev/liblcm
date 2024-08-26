aa682dddae43e211f851e1e7be2b7bd959da52e3
John T Maxwell III <maxwellparc@gmail.com>
2024-08-26 09:35:51 -0700

    Fix LT-21870 and LT-21872 and boundary markers (#311)
    * Fix LT-21872 (Import Phonology crashes) and boundary marker problem
    * Add DeletePhonology, fix LT-21870 (Import phonology crash)
    * Fix duplicate GUID problem

--- src/SIL.LCModel/Application/ApplicationServices/XmlImportData.cs
+++ src/SIL.LCModel/Application/ApplicationServices/XmlImportData.cs
@@ -995,12 +995,28 @@ namespace SIL.LCModel.Application.ApplicationServices
 					cmo = m_cache.LangProject.LexDbOA;
 					Debug.Assert(cmo != null);
 					break;
+				case "PhBdryMarker":
+					IPhBdryMarkerRepository repository = m_cache.ServiceLocator.GetInstance<IPhBdryMarkerRepository>();
+					IPhBdryMarkerFactory factory = m_cache.ServiceLocator.GetInstance<IPhBdryMarkerFactory>();
+					Guid guid = Guid.Parse(xrdr.GetAttribute("Guid"));
+					IPhBdryMarker marker;
+					if (repository.TryGetObject(guid, out marker))
+						cmo = marker;
+					else
+						cmo = factory.Create(guid, (IPhPhonemeSet)fi.Owner);
+					// Remove the default code added by PhTerminalUnit.SetDefaultValuesAfterInit in OverridesLing_Lex.
+					(cmo as PhBdryMarker).CodesOS.Clear();
+					break;
 				case "PhPhonData":
 					cmo = m_cache.LangProject.PhonologicalDataOA;
 					Debug.Assert(cmo != null);
 					break;
 				case "PhFeatureSystem":
 					cmo = m_cache.LangProject.PhFeatureSystemOA;
+					Debug.Assert(cmo != null);
+					break;
+				case "PhPhonemeSet":
+					cmo = m_cache.LangProject.PhonologicalDataOA.GetPhonemeSet();
 					Debug.Assert(cmo != null);
 					break;
 				default:
@@ -1048,7 +1064,6 @@ namespace SIL.LCModel.Application.ApplicationServices
 						cmo = m_repoCmObject.GetObject(hvo);
 						// Remove the default code added by PhTerminalUnit.SetDefaultValuesAfterInit in OverridesLing_Lex.
 						(cmo as PhPhoneme)?.CodesOS.Clear();
-						(cmo as PhBdryMarker)?.CodesOS.Clear();
 						// Remove default values.
 						(cmo as PhRegularRule)?.RightHandSidesOS.Clear();
 					}
