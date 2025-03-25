4f1aa51f783f203369e7fd8870ff06557b0f28c4
Kevin Hahn <kevin_hahn@sil.org>
2025-03-25 22:57:37 +0700

    migrate from NHunspell to WeCantSpell.Hunspell (#309)
    * integrate WeCantSpell into liblcm
    
    * remove the forced sdk version 6 as our tests need to run on dotnet 8
    
    * disable assembly signing which causes lcm to require strong named dependencies
    
    * remove NHunspell
    
    * removed unused sign key file
    
    ---------
    
    Co-authored-by: Jake Oliver <jeoliver97@gmail.com>

---
+++ src/SIL.LCModel.Core/SpellChecking/SpellEngineWeCantSpell.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Icu;
+using WeCantSpell.Hunspell;
+
+namespace SIL.LCModel.Core.SpellChecking;
+
+internal class SpellEngineWeCantSpell(string affixPath, string dictPath, string exceptionPath)
+	: SpellEngine(exceptionPath)
+{
+	private readonly WordList _wordList = WordList.CreateFromFiles(dictPath, affixPath);
+
+	public override bool Check(string word)
+	{
+		var normalized = Normalizer.Normalize(word, Normalizer.UNormalizationMode.UNORM_NFC);
+		return _wordList.Check(normalized);
+	}
+
+	public override ICollection<string> Suggest(string badWord)
+	{
+		var result =  _wordList.Suggest(badWord);
+		return result as ICollection<string> ?? result.ToArray();
+	}
+
+	protected override void SetStatusInternal(string word1, bool isCorrect)
+	{
+		if (isCorrect)
+		{
+			var detail = IsVernacular
+				? new WordEntryDetail(
+					FlagSet.Create(new FlagValue(SpellingHelper.keepCaseFlag)),
+					MorphSet.Create([SpellingHelper.PrototypeWord]),
+					WordEntryOptions.None
+				)
+				: WordEntryDetail.Default;
+			_wordList.Add(word1, detail);
+		}
+		else
+		{
+			_wordList.Remove(word1);
+		}
+	}
+}
