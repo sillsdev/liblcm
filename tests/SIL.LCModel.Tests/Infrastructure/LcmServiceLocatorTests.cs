3a708003078effa92bf71f6622a36ab197479546
Kevin Hahn <kevin_hahn@sil.org>
2026-07-22 14:20:38 +0700

    Add smoke tests for LCM service locator functionality
    - Introduced LcmServiceLocatorTests to verify core services resolution and instance retrieval.
    - Added tests for checking registered services and handling unregistered types.

---
+++ tests/SIL.LCModel.Tests/Infrastructure/LcmServiceLocatorTests.cs
@@ -0,0 +1,59 @@
+// Copyright (c) 2026 SIL International
+// This software is licensed under the LGPL, version 2.1 or later
+// (http://www.gnu.org/licenses/lgpl-2.1.html)
+
+using System.Linq;
+using NUnit.Framework;
+using SIL.LCModel.Core.KernelInterfaces;
+
+namespace SIL.LCModel.Infrastructure
+{
+	/// <summary>
+	/// Smoke tests for the LCM service locator wiring.
+	/// </summary>
+	[TestFixture]
+	public class LcmServiceLocatorTests : MemoryOnlyBackendProviderTestBase
+	{
+		/// <summary>
+		/// Verify that core services resolve and shortcut properties match GetInstance.
+		/// </summary>
+		[Test]
+		public void SmokeTest_ResolvesCoreServices()
+		{
+			var sl = Cache.ServiceLocator;
+			Assert.That(sl, Is.Not.Null);
+
+			Assert.That(sl.GetInstance<IDataSetup>(), Is.Not.Null);
+			Assert.That(sl.GetInstance<ICmObjectRepository>(), Is.Not.Null);
+			Assert.That(sl.GetInstance<IFwMetaDataCacheManaged>(), Is.Not.Null);
+
+			Assert.That(sl.DataSetup, Is.SameAs(sl.GetInstance<IDataSetup>()));
+			Assert.That(sl.ObjectRepository, Is.SameAs(sl.GetInstance<ICmObjectRepository>()));
+			Assert.That(sl.MetaDataCache, Is.SameAs(sl.GetInstance<IFwMetaDataCacheManaged>()));
+			Assert.That(sl.ActionHandler, Is.SameAs(sl.GetInstance<IActionHandler>()));
+		}
+
+		/// <summary>
+		/// GetAllInstances returns registered services and an empty sequence for unknown types.
+		/// </summary>
+		[Test]
+		public void GetAllInstances_ReturnsRegisteredServices()
+		{
+			var instances = Cache.ServiceLocator.GetAllInstances<IDataSetup>().ToList();
+			Assert.That(instances, Is.Not.Empty);
+			Assert.That(instances[0], Is.SameAs(Cache.ServiceLocator.GetInstance<IDataSetup>()));
+		}
+
+		/// <summary>
+		/// GetAllInstances returns an empty sequence when the type is not registered.
+		/// </summary>
+		[Test]
+		public void GetAllInstances_ReturnsEmptyForUnregisteredType()
+		{
+			var instances = Cache.ServiceLocator.GetAllInstances<IUnregisteredForServiceLocatorTest>().ToList();
+			Assert.That(instances, Is.Empty);
+		}
+
+		private interface IUnregisteredForServiceLocatorTest { }
+	}
+}
